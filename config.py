18adace198f815969dee5b45cb94e98807f266e6
crazyworld-izzy <126413936+crazyworld-izzy@users.noreply.github.com>
2023-07-15 13:06:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", None)
+TOKEN = getenv("TOKEN", "5995538130:AAEN9hTOGr0P_zYBsRWjmwq1g35uUS4ef_8")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://bsdk:betichod@cluster0.fgj1r9z.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://moni1:ammu@cluster0.lmgyalw.mongodb.net/?retryWrites=true&w=majority",
 )
