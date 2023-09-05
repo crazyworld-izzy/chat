055d3c1c23fd0b20db5a7c648241eb0e76d78286
crazyworld-izzy <126413936+crazyworld-izzy@users.noreply.github.com>
2023-09-05 17:11:03 +0530

    Update Start.py

--- AsuX/modules/Start.py
+++ AsuX/modules/Start.py
@@ -4,7 +4,7 @@
 """
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import CommandHandler, ContextTypes
+from telegram.ext import CommandHandler, ContextTypes, Updater
 
 from AsuX import rani
 
