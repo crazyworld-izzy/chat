51989e8c83a76dace823b7d5a7cc57aa1ff548ed
crazyworld-izzy <126413936+crazyworld-izzy@users.noreply.github.com>
2023-09-05 23:18:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ TOKEN = getenv("TOKEN", "6009759960:AAHUnDKep8-A9uuIKmEAnPIFrneaOIUsVGk")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://Dada:izzyyir@143@cluster0.r3nby2c.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://Dada:Dada@cluster0.r3nby2c.mongodb.net/?retryWrites=true&w=majority",
 )
