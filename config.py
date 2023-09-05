b37561c66c2da6932b08a6fc781b7e9254ff2b2e
crazyworld-izzy <126413936+crazyworld-izzy@users.noreply.github.com>
2023-09-05 17:14:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "6009759960:AAFuGOygY1AfHNW2IFXpESf2EQjx5JjHFoo")
+TOKEN = getenv("TOKEN", "6009759960:AAHUnDKep8-A9uuIKmEAnPIFrneaOIUsVGk")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
