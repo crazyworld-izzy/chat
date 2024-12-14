3c29bba57fcaec88a2d259e72eb3cc47c8d1d9b5
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-12-15 01:15:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ TOKEN = getenv("TOKEN","7817669190:AAFiCkLQAKItlA4uhbqO_hlpRfnDLr6my-Q")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://ravi:ravi12345@cluster0.hndinhj.mongodb.net/?retryWrites=true&w=majority",
 )
